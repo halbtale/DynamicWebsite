9484555b650d84c7a3ad6eb9f66ea2a09180bcf4
Alberto <alberto@MacBook-Air-di-Alberto.local>
2019-11-25 20:49:03 +0100

    Temporany FIX

--- express/database/connectDb.js
+++ express/database/connectDb.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// const mongoose = require('mongoose');
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log('Db Connection Successful');
-    })
-    .catch(error => {
-        console.log(error);
-    });
+// mongoose
+//     .connect(DB, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log('Db Connection Successful');
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+console.log('IT IS HERE');
