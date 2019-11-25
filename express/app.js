5159c77b2c8fdddc2ffec1b19450164ab298bb13
Alberto <alberto@MacBook-Air-di-Alberto.local>
2019-11-25 20:44:17 +0100

    Updated API

--- express/app.js
+++ express/app.js
@@ -6,31 +6,19 @@ const commentsRouter = require('./routes/commentsRouter');
 dotenv.config({ path: `${__dirname}/config.env` });
 
 // connectDB
-// require('./database/connectDB');
+require('./database/connectDB');
 
 //initializate express app
 const app = express();
 
 // Body parser
-// app.use(express.json());
+app.use(express.json());
 
 // Serving static files
 app.use(express.static(`${__dirname}/../`));
 
 // Api routers
-// app.use('/.netlify/functions/api/v1/comments', commentsRouter);
-
-app.get('/.netlify/functions/app', (req, res) => {
-    res.json({
-        status: 'success',
-        data: [
-            {
-                author: 'Giovanni',
-                message: 'Boh'
-            }
-        ]
-    });
-});
+app.use('/.netlify/functions/app', commentsRouter);
 
 module.exports = app;
 module.exports.handler = serverless(app);
