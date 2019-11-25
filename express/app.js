ef16adbb05db869c55cbcb935cddf419937b0c20
Alberto <alberto@MacBook-Air-di-Alberto.local>
2019-11-25 20:35:07 +0100

    Testing Purpose

--- express/app.js
+++ express/app.js
@@ -6,19 +6,31 @@ const commentsRouter = require('./routes/commentsRouter');
 dotenv.config({ path: `${__dirname}/config.env` });
 
 // connectDB
-require('./database/connectDB');
+// require('./database/connectDB');
 
 //initializate express app
 const app = express();
 
 // Body parser
-app.use(express.json());
+// app.use(express.json());
 
 // Serving static files
 app.use(express.static(`${__dirname}/../`));
 
 // Api routers
-app.use('/.netlify/functions/api/v1/comments', commentsRouter);
+// app.use('/.netlify/functions/api/v1/comments', commentsRouter);
+
+app.get('/.netlify/functions/api/v1/comments', (req, res) => {
+    res.json({
+        status: 'success',
+        data: [
+            {
+                author: 'Giovanni',
+                message: 'Boh'
+            }
+        ]
+    });
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
